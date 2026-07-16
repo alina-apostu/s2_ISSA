698249fc158a0636a374dc514393b0ca8e71cdc2
Alina Apostu <alina.apostu@student.tuiasi.ro>
2026-07-16 16:44:42 +0300

    set2, ex8

---
+++ set2/ex8.py
@@ -0,0 +1,26 @@
+def f(*liste):
+    lung_max=0
+    list_max=0
+    rez=[]
+    for l in liste:
+        if len(l)>lung_max:
+            lung_max=len(l)
+            list_max=l
+    for i in range(0,lung_max):
+        list_temp=[]
+        for l in liste:
+            if i<=lung_max:
+                list_temp.append(l[i])
+            elif i>=lung_max:
+                list_temp.append(None)
+
+        rez.append(list_temp)
+    return rez
+
+if __name__=='__main__':
+    a=[1,2,3]
+    b=[5,6,7]
+    c=["a", "b", "c"]
+    liste=[a,b,c]
+    rez=f(*liste)
+    print(rez)
